673fb74a84dae6868efd52ce7d74f859b92698a1
tilak tiwari <Tilaktiwari789@gmail.com>
2025-11-09 14:12:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "tsiem-yq8ox",
+  "appId": "1:578088327127:web:c80a85f34a083f08705d92",
+  "apiKey": "AIzaSyBiVUBKu6Ckf10FuqLSfJS9r4ct-WGOXts",
+  "authDomain": "tsiem-yq8ox.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "578088327127"
+};
